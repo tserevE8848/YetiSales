6068d41a62b5611a59ecc281db377d0bac9a8c67
tserevE8848 <88194407+tserevE8848@users.noreply.github.com>
2021-08-28 23:53:22 +1000

    Update YetiSales.js

--- YetiSales.js
+++ YetiSales.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { Client, Intents } = require('discord.js');
-
+var ethers = require('ethers');
+const sdk = require('api')('@opensea/v1.0#10ly3a2fkr6dkwq4');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -8,6 +9,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+	channel = client.channels.fetch(process.env.DISCORD_CHANNEL_ID);
 });
 
 // Login to Discord with your client's token
